47d4c7c6b5c10e2a53645baa5e2d6647694d6c6c
Nate Miller <NKDMiller96@gmail.com>
2018-12-06 19:37:46 -0600

    crete stock table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_033518) do
+ActiveRecord::Schema.define(version: 2018_12_07_013639) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "symbol"
+    t.float "price"
+    t.float "change"
+    t.string "company_name"
+    t.string "sector"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
