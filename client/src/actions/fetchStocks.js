64019bc63f1230816e98c3438a00171c562b7dd4
nate <natekmiller96@gmail.com>
2020-06-28 23:10:42 -0400

    update fetchStocks.js to clear error on success

--- client/src/actions/fetchStocks.js
+++ client/src/actions/fetchStocks.js
@@ -2,7 +2,12 @@ export function fetchStocks() {
   return (dispatch, getState) => {
     dispatch({ type: 'LOADING_STOCKS' });
     return fetch("https://api.iextrading.com/1.0/stock/"+getState().stocks.targetStock+"/book")
-      .then(response => response.json())
+      .then(response => {
+        if (response.ok) {
+          document.getElementById("error-handler").innerHTML = '';
+        }
+        return response.json()
+      })
       .then(stocks => dispatch({ type: 'ADD_STOCK_INFO', stocks }))
       .catch(e => document.getElementById("error-handler").innerHTML = 'Stock not found. Please try again.');
   };
